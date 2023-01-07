da23339c4f11491f8539345a5a95eca86510e705
Vincentvunguyen <luannguyenvu@gmail.com>
2023-01-07 01:00:10 -0500

    update

--- day5/exe02.py
+++ day5/exe02.py
@@ -1,4 +1,5 @@
-students = [["SV001", "Bob", 23], ["SV002", "Kenny", 34], ["SV003", "Henry", 45]]
+students = [["SV001", "Bob", 23], [
+    "SV002", "Kenny", 34], ["SV003", "Henry", 45]]
 
 # Lấy ra thông tin của sinh viên thứ nhất và in ra định dạng "ID: {id}, name: {name} - age: {age}"
 id1 = students[0][0]
@@ -13,8 +14,10 @@ print(age2)
 # Lấy ra thông tin hai sinh viên cuối cùng
 new_students = students[-2:]
 print(new_students)
-print(f"""ID: {students[1][0]}, name: {students[1][1]} - age: {students[1][2]}""")
-print(f"""ID: {students[2][0]}, name: {students[2][1]} - age: {students[2][2]}""")
+print(
+    f"""ID: {students[1][0]}, name: {students[1][1]} - age: {students[1][2]}""")
+print(
+    f"""ID: {students[2][0]}, name: {students[2][1]} - age: {students[2][2]}""")
 
 #  Lấy ra id của sinh viên thứ ba
 id3 = students[2][0]
