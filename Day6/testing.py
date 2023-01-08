5158ea152f5c17310f929dc1d579cbd014c39309
Vincentvunguyen <luannguyenvu@gmail.com>
2023-01-08 13:59:01 -0500

    Submit bai tap Day 6

---
+++ Day6/testing.py
@@ -0,0 +1,24 @@
+# Tạo set trống
+set_a = set()
+
+# Thêm 'Anna' vào set_a
+set_a.add("Anna")
+print(set_a)
+
+# Thêm một tuple ('Kenny', 'Jen', 'Danny') 
+# Trường hợp dùng hàm add thay vì update
+name = ('Kenny', 'Jen', 'Danny')
+print(type(name))
+set_a.add(name)
+
+# In ra set_a và tính chiều dài của nó
+print(set_a)
+print(len(set_a))
+
+# Xóa 'Jen' ra khỏi set_a
+set_a.remove("Jen")
+print(set_a)
+
+# Xóa tất cả các giá trị từ set_a
+set_a.clear()
+print(set_a)
