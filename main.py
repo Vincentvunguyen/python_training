31894434c390a0e132410afdcfa7dd4a1b9b8d7f
Vincentvunguyen <luannguyenvu@gmail.com>
2023-01-26 00:29:07 -0500

    Submit Day11

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-# Ctrl + ,
-# Win + .
-print("a",end='')
-print("b")
+     
+def print_show_info(tv_show):
+    print(f"{tv_show['title']} ({tv_show['initial_release']}) - {tv_show['seasons']} seasons")
+
+info = {
+    "title": "Breaking Bad",
+    "seasons": 5,
+    "initial_release": 2008
+}
+
+print_show_info(info)
